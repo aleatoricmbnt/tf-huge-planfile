5e122d94e7aa7429079f520959ca199173599782
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-09-11 23:01:22 +0300

    overall count 9 -> 24

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "some" {
-  count = 3
+  count = 8
   triggers = {
     long = var.long_var_1
     # another_long = var.long_var_2
@@ -8,7 +8,7 @@ resource "null_resource" "some" {
 }
 
 resource "random_pet" "test" {
-  count = 3
+  count = 8
   keepers = {
     long = var.long_var_1
     # another_long = var.long_var_2
@@ -17,7 +17,7 @@ resource "random_pet" "test" {
 }
 
 resource "random_password" "name" {
-  count = 3
+  count = 8
   length = 6
   keepers = {
     long = var.long_var_1
@@ -27,7 +27,7 @@ resource "random_password" "name" {
 }
 
 # resource "random_string" "random" {
-#   count = 30000
+#   count = 80000
 
 #   length           = 6
 #   special          = true
