23e21ffa191d19f483c1ba0d552fd34a098ec566
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-09-11 22:53:32 +0300

    commented variable

--- main.tf
+++ main.tf
@@ -1,33 +1,33 @@
 resource "null_resource" "some" {
-  count = 3
+  count = 5
   triggers = {
     long = var.long_var_1
-    another_long = var.long_var_2
+    # another_long = var.long_var_2
     # yet_another_long = var.long_var_3
   }
 }
 
 resource "random_pet" "test" {
-  count = 3
+  count = 5
   keepers = {
     long = var.long_var_1
-    another_long = var.long_var_2
+    # another_long = var.long_var_2
     # yet_another_long = var.long_var_3
   }
 }
 
 resource "random_password" "name" {
-  count = 3
+  count = 5
   length = 6
   keepers = {
     long = var.long_var_1
-    another_long = var.long_var_2
+    # another_long = var.long_var_2
     # yet_another_long = var.long_var_3
   }
 }
 
 # resource "random_string" "random" {
-#   count = 30000
+#   count = 50000
 
 #   length           = 6
 #   special          = true
