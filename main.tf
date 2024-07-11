abb1383b85c33b0262f4a3b67a2bc4f304d8473b
Mariia Shytse <aleatoricmbnt@gmail.com>
2024-07-11 14:36:55 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "some" {
-  count = 4
+  count = 1
   triggers = {
     long = var.long_var_1
     # another_long = var.long_var_2
@@ -8,7 +8,7 @@ resource "null_resource" "some" {
 }
 
 resource "random_pet" "test" {
-  count = 4
+  count = 0
   keepers = {
     long = var.long_var_1
     # another_long = var.long_var_2
@@ -17,7 +17,7 @@ resource "random_pet" "test" {
 }
 
 resource "random_password" "name" {
-  count = 4
+  count = 0
   length = 6
   keepers = {
     long = var.long_var_1
